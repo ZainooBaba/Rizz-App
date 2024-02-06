a0d1e69a8e4af582ee4acfc24874a4fee55bf4a6
ZainSiddi <135157583+ZainSiddi@users.noreply.github.com>
2024-02-06 01:17:16 -0600

    added evil rizz lines

--- src/rizzLogic.js
+++ src/rizzLogic.js
@@ -1,6 +1,11 @@
-import {rizzlines} from './rizzLines'
-export function createRizzResponse(name) {
+import {rizzlines, evilRizzlines} from './rizzLines'
+
+const BADLINE_CHANCE = 0.7
+export function createRizzResponse(name,profanityDisabled) {
   var line = rizzlines
+  if(!profanityDisabled && (Math.random() < BADLINE_CHANCE)){
+    line = evilRizzlines
+  }
   var lineNumber = Math.floor(Math.random()*rizzlines.length)
   var newLine = line[lineNumber].replace("[NAME]", name);
   return (newLine)
