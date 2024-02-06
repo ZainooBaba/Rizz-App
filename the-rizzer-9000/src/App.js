6d463484985abd9d1a0a3e04e5c40fe9ce240db3
Arvind Shamaraya <arvind.shamaraya04@gmail.com>
2024-02-06 00:03:29 -0600

    Update UI

--- the-rizzer-9000/src/App.js
+++ the-rizzer-9000/src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [userName, setUserName] = useState('');
+
+  const handleChange = (event) => {
+    setUserName(event.target.value);
+  }
+
+  const handleClick = () => {
+    alert("Hello, " + userName + "!");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>The Rizz App</h1>
+       <font size="5">Need Help With The Ladies/Gents? Try The Rizz App</font>
+       <input type="text" value={userName} onChange={handleChange} />
+       <button onClick={handleClick}>Enter</button>
       </header>
     </div>
   );
