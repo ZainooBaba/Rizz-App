f1c087608b626424f1439fe06d40b112d64b54a0
ZainSiddi <135157583+ZainSiddi@users.noreply.github.com>
2024-02-06 01:01:21 -0600

    made rizz logic more advanced

--- src/rizzLogic.js
+++ src/rizzLogic.js
@@ -1,3 +1,10 @@
+import {rizzlines} from './rizzLines'
 export function createRizzResponse(name) {
-  return ("whats up, " + name + "!")
+  var line = rizzlines
+  var lineNumber = Math.floor(Math.random()*rizzlines.length)
+  var newLine = line[lineNumber].replace("[NAME]", name);
+  return (newLine)
 }
+
+
+
