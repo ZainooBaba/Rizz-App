5bf86db8ac97438d78449b0fd27d84cc57ef26dd
Shardul149 <159119800+Shardul149@users.noreply.github.com>
2024-02-06 03:02:04 -0600

    Update rizzLines.js
    Added rizz line 183

--- src/rizzLines.js
+++ src/rizzLines.js
@@ -180,4 +180,5 @@ export const evilRizzlines = [
     "Wanna sploink?",
     "Holy shit I’m bout to expunge",
     "Ay Soulja boy wanna fuck",
+    "So is your name Gillette? Because you look like the best a man can get",
 ]
